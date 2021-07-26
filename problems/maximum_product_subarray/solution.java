be48810d008305aaefc68def06accd71a653c802
Lucas <aranlucas@gmail.com>
2021-07-26 14:56:44 +0000

    Sync LeetCode submission - Maximum Product Subarray (java)

---
+++ problems/maximum_product_subarray/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        var maxProduct = nums[0];
+        var minProduct = nums[0];
+        int response = nums[0];
+
+        for (var i = 1; i <= nums.length - 1; i++) {
+
+            int choice1 = minProduct * nums[i];
+            int choice2 = maxProduct * nums[i];
+            maxProduct = Math.max(nums[i], Math.max(choice1, choice2));
+            minProduct = Math.min(nums[i], Math.min(choice1, choice2));
+            response = Math.max(maxProduct, response);
+        }
+
+        return response;
+    }
+}
