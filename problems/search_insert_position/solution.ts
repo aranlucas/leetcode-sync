4521d976681560322545b585faf2a215cee482ac
Lucas <aranlucas@gmail.com>
2022-02-26 21:45:11 +0000

    Sync LeetCode submission - Search Insert Position (typescript)

---
+++ problems/search_insert_position/solution.ts
@@ -0,0 +1,21 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length -1;
+    
+    while (left<=right){
+        let mid = Math.floor((right+left)/2)
+        
+        if(nums[mid] === target){
+            return mid
+        }
+        
+        if(target < nums[mid])
+            right = mid - 1
+        else
+            left = mid + 1
+        
+        
+    }
+    return left
+
+};
