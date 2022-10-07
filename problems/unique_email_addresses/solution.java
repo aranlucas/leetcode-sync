a8011c0a535823c6d265fd9a4b1d919c2830ed5d
Lucas <aranlucas@gmail.com>
2022-10-07 21:01:03 +0000

    Sync LeetCode submission - Unique Email Addresses (java)

---
+++ problems/unique_email_addresses/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new HashSet<>();
+        
+        for (String email : emails){
+            String[] parts = email.split("@");
+            String[] local = parts[0].split("\\+");
+            
+            String cleanEmail = local[0].replace(".", "") + "@" + parts[1];
+            
+            uniqueEmails.add(cleanEmail);
+        }
+        
+        return uniqueEmails.size();
+    }
+}
