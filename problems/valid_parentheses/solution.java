14bd0537c85f686f20817c119c470539a66eb2a5
Lucas <aranlucas@gmail.com>
2022-10-14 22:08:41 +0000

    Sync LeetCode submission - Valid Parentheses (java)

---
+++ problems/valid_parentheses/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+    
+        Map<Character, Character> mapping = new HashMap<>();
+        
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+        
+        Stack<Character> stack = new Stack<Character>();
+
+        
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            // Mapping found
+            if(mapping.containsKey(c)){
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != mapping.get(c)){
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
