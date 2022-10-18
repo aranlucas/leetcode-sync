14c80b142a171d572f6654619fdacc7109cf1b53
Lucas <aranlucas@gmail.com>
2022-10-18 21:05:28 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- problems/climbing_stairs/solution.java
+++ problems/climbing_stairs/solution.java
@@ -4,10 +4,8 @@ class Solution {
             return 1;
         }
         int[] dp = new int[n + 1];
-
         dp[1] = 1;
         dp[2] = 2;
-
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
