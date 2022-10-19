6c7198be64989dff0e5bdbcd8544da24b7f97d38
Lucas <aranlucas@gmail.com>
2022-10-19 02:23:42 +0000

    Sync LeetCode submission - Middle of the Linked List (java)

--- problems/middle_of_the_linked_list/solution.java
+++ problems/middle_of_the_linked_list/solution.java
@@ -10,17 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head.next == null){
+        if (head.next == null){
             return head;
         }
         ListNode slow = head;
         ListNode fast = head;
         
-        while(fast!= null && fast.next != null){
-            
+        while (fast!= null && fast.next != null){
             slow = slow.next;
             fast= fast.next.next;
-            
         }
         return slow;
     }
