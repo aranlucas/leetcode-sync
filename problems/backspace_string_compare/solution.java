c40b0065a3f261845c90723c65cfdc48c030ee57
Lucas <aranlucas@gmail.com>
2022-10-25 01:54:03 +0000

    Sync LeetCode submission - Backspace String Compare (java)

---
+++ problems/backspace_string_compare/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        if (s.equals(t)){
+            return true;
+        }
+        
+        Deque<Character> sStack = new ArrayDeque<>();
+        Deque<Character> tStack = new ArrayDeque<>();
+        
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c != '#'){
+                sStack.addLast(c);
+            } else if (!sStack.isEmpty()){
+                sStack.removeLast();
+            }
+        }
+        
+        for (int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            
+            if(c != '#'){
+                tStack.addLast(c);
+            } else if(!tStack.isEmpty()){
+                tStack.removeLast();
+            }
+        }
+        
+        return String.valueOf(tStack).equals(String.valueOf(sStack));
+    }
+}
