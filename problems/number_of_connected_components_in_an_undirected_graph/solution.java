209d7b9c2dc37a430e9e8541f0f392d1db8f272e
Lucas <aranlucas@gmail.com>
2022-11-03 23:04:01 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (java)

---
+++ problems/number_of_connected_components_in_an_undirected_graph/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        //Store all edges in 'graph'.
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<Integer>());
+        }
+        
+        // Compute adjacency list
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        Set<Integer> visited = new HashSet();
+
+        int components = 0;
+        // using keySet() for iteration over keys
+        for (int node: graph.keySet()) {
+            if (!visited.contains(node)) {
+                components++;
+                dfs(graph, node, visited);
+            }
+        }
+        return components;
+    }
+    
+    public void dfs(Map<Integer, List<Integer>> graph, int startNode, Set<Integer> visited){
+        visited.add(startNode);
+        
+        var neighbors = graph.get(startNode);
+        
+        for (int neighbor: neighbors){
+            if(!visited.contains(neighbor)){
+                dfs(graph, neighbor, visited);
+            }
+        }
+    }
+}
