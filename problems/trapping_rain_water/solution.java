72ef53d5870d64f2d893639aa7a2d2b371cfae76
Lucas <aranlucas@gmail.com>
2022-11-08 02:57:24 +0000

    Sync LeetCode submission - Trapping Rain Water (java)

---
+++ problems/trapping_rain_water/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        
+        int maxLeft = 0;
+        int maxRight = 0;
+        int trappedRain = 0;
+        while (left < right) {
+            if(height[left]< height[right]){
+                int waterTrappedLeft = maxLeft - height[left];
+                if (waterTrappedLeft > 0){
+                    trappedRain +=waterTrappedLeft;
+                }
+                maxLeft = Math.max(height[left], maxLeft);
+                left++;
+
+            } else {
+                int waterTrappedRight = maxRight - height[right];
+                if(waterTrappedRight > 0){
+                    trappedRain += waterTrappedRight;
+                }
+                maxRight = Math.max(height[right], maxRight);
+                right--;
+
+            }
+            
+        }
+        
+        return trappedRain;
+    }
+}
