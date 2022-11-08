6ea2986ddeb988c67ed91289f8419598042a855a
Lucas <aranlucas@gmail.com>
2022-11-08 01:46:07 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- problems/valid_palindrome/solution.java
+++ problems/valid_palindrome/solution.java
@@ -5,19 +5,24 @@ class Solution {
         int right = s.length() -1;
         
         while (left <= right){
+            // Check if left is not letter, increase
             if(!Character.isLetterOrDigit(s.charAt(left))){
                 left++;
                 continue;
             }
+            
+            // Check if right is not letter, decrease
             if(!Character.isLetterOrDigit(s.charAt(right))){
                 right--;
                 continue;
             }
             
+            // Check if they right and left are equal to each other
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
                 return false;
             }
             
+            // Incremenet both;
             left++;
             right--;
         }
