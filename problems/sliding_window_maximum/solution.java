c398fb6abc1e2c4af99e8119aa01a44dbf548841
Lucas <aranlucas@gmail.com>
2022-11-08 01:28:38 +0000

    Sync LeetCode submission - Sliding Window Maximum (java)

---
+++ problems/sliding_window_maximum/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length - k + 1];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
+        
+        int windowStart = 0;
+        for(int windowEnd = 0; windowEnd < nums.length; windowEnd++){
+            int currentNum = nums[windowEnd];
+
+            pq.add(new int[]{nums[windowEnd],windowEnd});
+            
+            if (windowEnd - windowStart + 1 == k) {
+                int[] max = pq.peek();
+                while(!pq.isEmpty() && max[1] < windowStart){
+                    pq.poll();
+                    max = pq.peek();
+                }
+                ans[windowStart] = max[0];
+                windowStart++;
+            }
+        }
+        return ans;        
+    }
+}
