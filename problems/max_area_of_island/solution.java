45e0ac8b5b28937b4c05f90c2171246f535587e7
Lucas <aranlucas@gmail.com>
2022-11-09 01:28:02 +0000

    Sync LeetCode submission - Max Area of Island (java)

--- problems/max_area_of_island/solution.java
+++ problems/max_area_of_island/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        
         Set<String> visited = new HashSet();
         
         int maxArea = 0;
         for(int r = 0; r < grid.length; r++){
             for(int c = 0; c < grid[0].length; c++){
                 String key = key(r,c);
-                if(grid[r][c] != 0 && !visited.contains(key)){
+                if (grid[r][c] != 0 && !visited.contains(key)){
                     maxArea = Math.max(maxArea, dfs(r, c, grid, visited));
                 }
             }
@@ -26,26 +25,26 @@ class Solution {
         
         String key = key(r,c);
         
-        if(visited.contains(key)){
+        if (visited.contains(key)){
             return 0;
         }
         
-        if(graph[r][c] == 0){
+        if (graph[r][c] == 0){
             return 0;
         }
         
         visited.add(key);
         
         int area = 1;
-        area+=dfs(r+1,c,graph,visited);
-        area+=dfs(r-1,c,graph,visited);
-        area+=dfs(r,c+1,graph,visited);
-        area+=dfs(r,c-1,graph,visited);
+        area += dfs(r+1,c,graph,visited);
+        area += dfs(r-1,c,graph,visited);
+        area += dfs(r,c+1,graph,visited);
+        area += dfs(r,c-1,graph,visited);
         
         return area;
     }
     
     public String key(int r, int c){
-        return r+","+ c;
+        return r + "," + c;
     }
 }
