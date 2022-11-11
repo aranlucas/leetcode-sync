29f374fc172c811b11eca6910a32e070fe38b112
Lucas <aranlucas@gmail.com>
2022-11-11 22:56:55 +0000

    Sync LeetCode submission - House Robber (java)

--- problems/house_robber/solution.java
+++ problems/house_robber/solution.java
@@ -9,7 +9,7 @@ class Solution {
         if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        var dp = new int[nums.length];
+        int[] dp = new int[nums.length];
 
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
