9269f48733848e6888e2381613c33bdb0e788056
Lucas <aranlucas@gmail.com>
2022-11-12 23:29:52 +0000

    Sync LeetCode submission - Insert Interval (java)

--- problems/insert_interval/solution.java
+++ problems/insert_interval/solution.java
@@ -1,44 +1,22 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int newStart = newInterval[0];
-        int newEnd = newInterval[1];
-        
-        
-        int idx = 0;
-        int n = intervals.length;
-        
-        LinkedList<int[]> output = new LinkedList<int[]>();
-        
-        // add all intervals before newInterval
-        while (idx < n && newStart > intervals[idx][0])
-            output.add(intervals[idx++]);
-        
-        // add newInterval
-        int[] interval = new int[2];
-        // if there is no overlap, just add the interval
-        if (output.isEmpty() || output.getLast()[1] < newStart)
-            output.add(newInterval);
-        // if there is an overlap, merge with the last interval
-        else {
-            interval = output.removeLast();
-            interval[1] = Math.max(interval[1], newEnd);
-            output.add(interval);
-        }
 
-        while (idx < n) {
-            interval = intervals[idx++];
-            int start = interval[0];
-            int end = interval[1];
-            
-            // No overlap
-            if (output.getLast()[1] < start){
-                output.add(interval);
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (newInterval == null || interval[1] < newInterval[0]) 
+                res.add(interval); 
+            else if (interval[0] > newInterval[1]) {
+                res.add(newInterval);
+                res.add(interval);
+                newInterval = null;
             } else {
-                interval = output.removeLast();
-                interval[1] = Math.max(interval[1], end);
-                output.add(interval);
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
             }
         }
-        return output.toArray(new int[output.size()][2]);
+        if (newInterval != null) {
+            res.add(newInterval);
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }
