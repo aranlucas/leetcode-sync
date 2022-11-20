0568967fc5a1f6cbf22986ddc3ad61fdce496152
Lucas <aranlucas@gmail.com>
2022-11-20 22:38:14 +0000

    Sync LeetCode submission - Max Area of Island (java)

--- problems/max_area_of_island/solution.java
+++ problems/max_area_of_island/solution.java
@@ -1,50 +1,46 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        Set<String> visited = new HashSet();
+        
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         
         int maxArea = 0;
-        for(int r = 0; r < grid.length; r++){
-            for(int c = 0; c < grid[0].length; c++){
-                String key = key(r,c);
-                if (grid[r][c] != 0 && !visited.contains(key)){
-                    maxArea = Math.max(maxArea, dfs(r, c, grid, visited));
-                }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxArea = Math.max(dfs(grid, i, j, visited), maxArea);
             }
         }
+        
         return maxArea;
     }
     
-    public int dfs(int r, int c, int[][] graph, Set<String> visited){
-        if(r < 0 || r >= graph.length){
+    public int dfs(int[][] grid, int r, int c, boolean[][] visited) {
+        // Out of bound checks
+        if (r < 0 || r >= grid.length) {
             return 0;
         }
         
-        if(c < 0 || c >= graph[0].length){
+        if (c < 0 || c >= grid[0].length) {
             return 0;
         }
         
-        String key = key(r,c);
-        
-        if (visited.contains(key)){
+        // Check if visited
+        if (visited[r][c]) {
             return 0;
         }
         
-        if (graph[r][c] == 0){
+        // Water check
+        if (grid[r][c] == 0){
             return 0;
         }
         
-        visited.add(key);
-        
+        visited[r][c] = true;
         int area = 1;
-        area += dfs(r+1,c,graph,visited);
-        area += dfs(r-1,c,graph,visited);
-        area += dfs(r,c+1,graph,visited);
-        area += dfs(r,c-1,graph,visited);
         
+        area += dfs(grid, r + 1, c, visited);
+        area += dfs(grid, r - 1, c, visited);
+        area += dfs(grid, r, c + 1, visited);
+        area += dfs(grid, r, c - 1, visited);
+
         return area;
-    }
-    
-    public String key(int r, int c){
-        return r + "," + c;
     }
 }
