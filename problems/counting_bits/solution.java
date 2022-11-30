a7733ee1613e2b8217378f508008d47f777356bb
Lucas <aranlucas@gmail.com>
2022-11-30 21:17:44 +0000

    Sync LeetCode submission - Counting Bits (java)

---
+++ problems/counting_bits/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] answer = new int[n + 1];
+        
+        
+        for (int i = n; i >= 0; i--){
+            answer[i] = count(i);
+        }
+        
+        return answer;
+    }
+    
+    private int count(int x) {
+        int count = 0;
+        while (x != 0) {
+            x &= x - 1;
+            count++;
+        }
+        return count;
+    }
+}
