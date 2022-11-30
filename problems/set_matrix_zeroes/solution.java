18d4b0b7f66b325281042a1df28d4cab36173844
Lucas <aranlucas@gmail.com>
2022-11-30 21:02:44 +0000

    Sync LeetCode submission - Set Matrix Zeroes (java)

---
+++ problems/set_matrix_zeroes/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        Set<Integer> zeroColumn = new HashSet();
+        Set<Integer> zeroRow = new HashSet();
+        
+        // Find all the zero's
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRow.add(i);
+                    zeroColumn.add(j);
+                    System.out.println(i + "," + j);
+                }
+            }
+        }
+        
+                // Find all the zero's
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (zeroRow.contains(i) || zeroColumn.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
