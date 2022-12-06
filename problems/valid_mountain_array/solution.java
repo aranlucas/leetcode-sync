37391a76d22a89a62e9bb6dcfe23dd302d386ec2
Lucas <aranlucas@gmail.com>
2022-12-06 03:54:24 +0000

    Sync LeetCode submission - Valid Mountain Array (java)

---
+++ problems/valid_mountain_array/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        
+        boolean isIncreasing = false;
+        boolean isDecreasing = false;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] == arr[i]) {
+                return false;
+            }
+            if (arr[i-1] < arr[i]) {
+                // cannot have a increase after decrease
+                if (isDecreasing == true) {
+                    return false;
+                }
+                isIncreasing = true;
+            } else if (arr[i-1] > arr[i]) {
+                isDecreasing = true;
+            }
+        }
+        
+        return isIncreasing && isDecreasing;
+    }
+}
