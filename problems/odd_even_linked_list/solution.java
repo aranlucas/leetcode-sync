5e14ff7eea9e26d5350d3fa3dae09175b504a7d9
Lucas <aranlucas@gmail.com>
2022-12-06 02:54:52 +0000

    Sync LeetCode submission - Odd Even Linked List (java)

---
+++ problems/odd_even_linked_list/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode odd = head; 
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+
+        while (even != null && even.next != null) {
+            // Put odd to the odd list
+            odd.next = odd.next.next;
+            
+            // Put even to the even list
+            even.next = even.next.next;
+
+            // Move the pointer to the next odd/even
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
