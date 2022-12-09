c6ec2d702384e5d5ff79990459d16add5416cba6
Lucas <aranlucas@gmail.com>
2022-12-09 00:21:13 +0000

    Sync LeetCode submission - Third Maximum Number (java)

---
+++ problems/third_maximum_number/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<Integer>();
+            
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            
+            if (set.size() == 3) {
+                if (set.first() < nums[i]) {
+                    // Remove first
+                    set.pollFirst();
+                    set.add(nums[i]);
+                }
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        
+        // If sorted set has three elements return the smallest among those 3.
+        if (set.size() == 3) {
+            return set.first();
+        }
+        return set.last();
+    }
+}
