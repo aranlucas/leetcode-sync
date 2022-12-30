4e5db7a03c2660120fe2e1d7d19da8dcc433e255
Lucas <aranlucas@gmail.com>
2022-12-30 18:03:59 +0000

    Sync LeetCode submission - All Paths From Source to Target (java)

---
+++ problems/all_paths_from_source_to_target/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+
+        int target = graph.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        path.addLast(0);
+
+        backtrack(0, path, graph, target, result);
+
+        return result;
+    }
+
+    public void backtrack(int currNode, LinkedList<Integer> path, int[][] graph, int target, List<List<Integer>> result) {
+        if (currNode == target) {
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
+
+        for (int nextNode: graph[currNode]) {
+            path.add(nextNode);
+            backtrack(nextNode, path, graph, target, result);
+            path.removeLast();
+        }
+    }
+}
