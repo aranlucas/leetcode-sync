03907bb32036d0a4185dff38cca5dc6e0624c31e
Lucas <aranlucas@gmail.com>
2023-01-28 01:12:20 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- problems/valid_parentheses/solution.java
+++ problems/valid_parentheses/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
+        Map<Character, Character> p = new HashMap<>();
+        p.put('}', '{');
+        p.put(']', '[');
+        p.put(')', '(');
 
-        Map<Character, Character> mapping = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
 
-        mapping.put(')', '(');
-        mapping.put('}', '{');
-        mapping.put(']', '[');
-
-        Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            // Mapping found
-            if (mapping.containsKey(c)) {
-                char topElement = stack.empty() ? '#' : stack.pop();
-                if (topElement != mapping.get(c)) {
+        for(char c : s.toCharArray()) {
+            if (p.containsKey(c)) {
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.isEmpty() ? '#' : stack.pop();   
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != p.get(c)) {
                     return false;
                 }
             } else {
-                stack.push(c);
+                stack.add(c);
             }
         }
 
