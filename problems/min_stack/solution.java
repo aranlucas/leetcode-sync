dc017d61c221f2b033f85d3e3eeebf802a5c2b0c
Lucas <aranlucas@gmail.com>
2023-01-28 01:31:52 +0000

    Sync LeetCode submission - Min Stack (java)

--- problems/min_stack/solution.java
+++ problems/min_stack/solution.java
@@ -1,35 +1,39 @@
 class MinStack {
 
-    Deque<int[]> stack;
+    Stack<int[]> stack;
 
     public MinStack() {
-        stack = new ArrayDeque<int[]>();
+        this.stack = new Stack<>();
     }
-
+    
     public void push(int val) {
         if (stack.isEmpty()) {
-            stack.push(new int[] {val, val});
+            stack.push(new int[] { val, val });
             return;
         }
-
-        int currentMin = stack.peek()[1];
-        stack.push(new int[] {val, Math.min(val, currentMin)});
+        
+        int currentMin = getMin();
+        stack.push(new int[] { val, Math.min(val, currentMin)});
     }
-
+    
     public void pop() {
         stack.pop();
     }
-
+    
     public int top() {
         return stack.peek()[0];
     }
-
+    
     public int getMin() {
         return stack.peek()[1];
     }
 }
 
 /**
- * Your MinStack object will be instantiated and called as such: MinStack obj = new MinStack();
- * obj.push(val); obj.pop(); int param_3 = obj.top(); int param_4 = obj.getMin();
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
