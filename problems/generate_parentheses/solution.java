f0fc8ec1ba4e91b7579bf90cf77ed39f02fdf74a
Lucas <aranlucas@gmail.com>
2023-01-28 02:06:59 +0000

    Sync LeetCode submission - Generate Parentheses (java)

--- problems/generate_parentheses/solution.java
+++ problems/generate_parentheses/solution.java
@@ -1,29 +1,50 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList();
-        StringBuilder current = new StringBuilder();
-        backtrack(ans, current, 0, 0, n);
-        return ans;
+
+        List<String> result = new ArrayList<>();
+        backtrack(n, new StringBuilder(), result);
+        return result;
     }
 
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
-        // Base case
-        if (cur.length() == max * 2) {
-            ans.add(cur.toString());
+    public void backtrack(int n, StringBuilder curr, List<String> result) {
+        if (curr.length() == n * 2) {
+            String s = curr.toString();
+            if (isValid(s)) {
+                result.add(s);
+            }
             return;
         }
 
-        // 2 choices. We can start an opening bracket if we still have one (of n) left to place
-        if (open < max) {
-            cur.append("(");
-            backtrack(ans, cur, open + 1, close, max);
-            cur.deleteCharAt(cur.length() - 1);
+        char[] candidates = new char[] { '(', ')'};
+
+        for (char c : candidates) {
+            curr.append(c);
+            backtrack(n, curr, result);
+            curr.deleteCharAt(curr.length() - 1);
         }
-        // We can start a closing bracket if it would not exceed the number of opening brackets.
-        if (close < open) {
-            cur.append(")");
-            backtrack(ans, cur, open, close + 1, max);
-            cur.deleteCharAt(cur.length() - 1);
+    }
+    
+    public boolean isValid(String s) {
+        Map<Character, Character> p = new HashMap<>();
+        p.put('}', '{');
+        p.put(']', '[');
+        p.put(')', '(');
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (p.containsKey(c)) {
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.isEmpty() ? '#' : stack.pop();   
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != p.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.add(c);
+            }
         }
+
+        return stack.isEmpty();
     }
 }
