73258a6283e9f6c37c854269ed176633b9b85b8a
Lucas <aranlucas@gmail.com>
2023-02-01 02:27:51 +0000

    Sync LeetCode submission - Permutation in String (java)

--- problems/permutation_in_string/solution.java
+++ problems/permutation_in_string/solution.java
@@ -1,39 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
-        Map<Character, Integer> s1Count = new HashMap();
+        Map<Character, Integer> s1Count = new HashMap<>();
 
         for (char c : s1.toCharArray()) {
             s1Count.put(c, s1Count.getOrDefault(c, 0) + 1);
         }
+        Map<Character, Integer> windowCount = new HashMap<>();
 
-        Map<Character, Integer> windowCount = new HashMap();
-
-        // Fixed window size problem
+        int left = 0;
         int k = s1.length();
-        int windowStart = 0;
-        for (int windowEnd = 0; windowEnd < s2.length(); windowEnd++) {
-            char r = s2.charAt(windowEnd);
+        for (int right = 0; right < s2.length(); right++) {
+            char r = s2.charAt(right);
             windowCount.put(r, windowCount.getOrDefault(r, 0) + 1);
-
-            // Window size is what we want, start moving left
-            if (windowEnd - windowStart + 1 == k) {
-                // Before removing windowStart, check if maps are equal
+            
+            while (right - left + 1 == k) {
                 if (areEqual(s1Count, windowCount)) {
                     return true;
                 }
-                char l = s2.charAt(windowStart);
-                windowCount.put(l, windowCount.getOrDefault(l, 0) - 1);
-                windowStart++;
+                char l = s2.charAt(left);
+                windowCount.put(l, windowCount.get(l) - 1);
+                left++;
             }
         }
 
         return false;
     }
-
+    
     private boolean areEqual(Map<Character, Integer> first, Map<Character, Integer> second) {
         return first.entrySet().stream().allMatch(e -> e.getValue().equals(second.get(e.getKey())));
     }
