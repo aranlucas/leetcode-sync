afadeb212220b22d06935c230beecb4486e44c2b
Lucas <aranlucas@gmail.com>
2023-02-01 03:10:04 +0000

    Sync LeetCode submission - Minimum Window Substring (java)

--- problems/minimum_window_substring/solution.java
+++ problems/minimum_window_substring/solution.java
@@ -1,69 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() == 0 || t.length() == 0) {
+        if (s.length() < t.length()) {
             return "";
         }
 
-        Map<Character, Integer> dictT = new HashMap();
-        // Count characters needed in t;
-        for (int i = 0; i < t.length(); i++) {
-            char tChar = t.charAt(i);
-            int count = dictT.getOrDefault(tChar, 0);
-            dictT.put(tChar, count + 1);
+        Map<Character, Integer> tCount = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            tCount.put(c, tCount.getOrDefault(c, 0) + 1);
         }
 
-        // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
-
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
-
-        // Left and Right pointer
-        int l = 0, r = 0;
-
-        // ans list of the form (window length, left, right)
-        int[] ans = {-1, 0, 0};
-
-        // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap();
-
-        while (r < s.length()) {
-            char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
+        int match = 0;
+        int required = tCount.size();
+        String answer = "";
+        int min = Integer.MAX_VALUE;
+        int left = 0;
+        Map<Character, Integer> windowCount = new HashMap<>();
+        for (int right = 0; right < s.length(); right++) {
+            char r = s.charAt(right);
+            windowCount.put(r, windowCount.getOrDefault(r, 0) + 1);
+            
             // If the frequency of the current character added equals to the
             // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
+            if (tCount.containsKey(r) && windowCount.get(r).intValue() == tCount.get(r).intValue()) {
+                match++;
             }
 
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = s.charAt(l);
-
-                // Save the smallest window until now. Special condition for first loop
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+            while (left <= right && match == required) {
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    answer = s.substring(left, right + 1);
                 }
-
-                // The character at the position pointed by the
-                // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c)
-                        && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
+                char l = s.charAt(left);
+                windowCount.put(l, windowCount.getOrDefault(l, 0) - 1);
+                
+                if (tCount.containsKey(l) && windowCount.get(l).intValue() < tCount.get(l).intValue()) {
+                    match--;
                 }
-                // Move the left pointer ahead, this would help to look for a new window.
-                l++;
+                left++;
             }
-            r++;
         }
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+
+        return answer;
     }
 }
