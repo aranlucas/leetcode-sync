2b19f2229bd4f3f4343601b9a0eb7452964b756f
Lucas <aranlucas@gmail.com>
2023-02-04 00:03:10 +0000

    Sync LeetCode submission - Reorder List (java)

--- problems/reorder_list/solution.java
+++ problems/reorder_list/solution.java
@@ -1,28 +1,36 @@
 /**
- * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {}
- * ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
- * this.next = next; } }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null) return;
+        ListNode dummy = head;
+        ListNode curr = head;
 
+        // Find middle - which should be slow
         ListNode slow = head;
         ListNode fast = head;
-
-        // Find middle node
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            System.out.println(slow.val);
         }
 
-        // reverse the second part of the list
-        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
-        // reverse the second half in-place
-        ListNode prev = null;
-        ListNode curr = slow;
-        ListNode tmp = null;
+        ListNode reverseList = reverseList(slow);
 
+        mergeLists(head, reverseList);
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode tmp = head;
         while (curr != null) {
             tmp = curr.next;
 
@@ -31,11 +39,11 @@ class Solution {
             curr = tmp;
         }
 
-        // merge two sorted linked lists [Problem 21]
-        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
-        ListNode first = head;
-        ListNode second = prev;
-
+        return prev;
+    }
+    
+    private void mergeLists(ListNode first, ListNode second) {
+        ListNode tmp = null;
         while (second.next != null) {
             tmp = first.next;
             first.next = second;
