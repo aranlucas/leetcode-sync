316e623b17fa6a2f9fcef3d73205c74ea14eca33
Lucas <aranlucas@gmail.com>
2023-02-22 23:08:01 +0000

    Sync LeetCode submission - Capacity To Ship Packages Within D Days (java)

---
+++ problems/capacity_to_ship_packages_within_d_days/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int totalLoad = 0;
+        int maxLoad = 0;
+
+        for (int weight : weights) {
+            totalLoad += weight;
+            maxLoad = Math.max(maxLoad, weight);
+        }
+
+        int l = maxLoad;
+        int r = totalLoad;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (feasible(weights, mid, days)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+
+    public boolean feasible(int[] weights, int c, int days) {
+        int daysNeeded = 1;
+        int currentLoad = 0;
+        for (int weight: weights) {
+            currentLoad += weight;
+
+            if (currentLoad > c) {
+                daysNeeded++;
+                currentLoad = weight;
+            }
+        }
+
+        return daysNeeded <= days;
+    }
+}
