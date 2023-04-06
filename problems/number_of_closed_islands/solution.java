d20d9c7f6734a3a4dd7ab2409a0b0e84cd67e1af
Lucas <aranlucas@gmail.com>
2023-04-06 21:16:05 +0000

    Sync LeetCode submission - Number of Closed Islands (java)

---
+++ problems/number_of_closed_islands/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public int closedIsland(int[][] grid) {
+        
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0 && !visited[i][j] && dfs(grid, i, j, visited)){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public boolean dfs(int[][] grid, int x, int y, boolean[][] visited) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return false;
+        }
+        
+        if (grid[x][y] == 1 || visited[x][y]){ 
+            return true;
+        };
+        
+        visited[x][y] = true;
+        
+        boolean res = true;
+        
+        for (int[] d : dir){
+            res = res & dfs(grid, x + d[0], y + d[1], visited);
+        }
+        
+        return res;
+    }
+}
