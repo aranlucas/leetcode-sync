73f62523929dad296ed4abb8929e64b847b6a968
Lucas <aranlucas@gmail.com>
2023-04-20 23:47:26 +0000

    Sync LeetCode submission - Reverse Words in a String (java)

--- problems/reverse_words_in_a_string/solution.java
+++ problems/reverse_words_in_a_string/solution.java
@@ -1,10 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-        // remove leading spaces
-        s = s.trim();
-        // split by multiple spaces
-        List<String> wordList = Arrays.asList(s.split("\\s+"));
-        Collections.reverse(wordList);
-        return String.join(" ", wordList);
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left <= right && s.charAt(left) == ' ') left++;
+        while (left <= right && s.charAt(right) == ' ') right--;
+
+        Deque<String> d = new ArrayDeque<>();
+
+        StringBuilder word = new StringBuilder();
+
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (c != ' ') {
+                word.append(c);
+            } else if ((word.length() != 0) && (c == ' ') ){
+                d.push(word.toString());
+                word.setLength(0);
+            }
+            left++;
+        }
+
+        d.push(word.toString());
+
+        return String.join(" ", d);
     }
 }
