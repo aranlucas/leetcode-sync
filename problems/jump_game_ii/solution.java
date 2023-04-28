4decb8c24007be4f15690bf4c1d9aaa1fd9d8a88
Lucas <aranlucas@gmail.com>
2023-04-28 23:50:07 +0000

    Sync LeetCode submission - Jump Game II (java)

--- problems/jump_game_ii/solution.java
+++ problems/jump_game_ii/solution.java
@@ -1,23 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        // The starting range of the first jump is [0, 0]
-        int answer = 0;
-        int n = nums.length;
-        int curEnd = 0;
-        int curFar = 0;
-
-        for (int i = 0; i < n - 1; ++i) {
-            // Update the farthest reachable index of this jump.
-            curFar = Math.max(curFar, i + nums[i]);
-
-            // If we finish the starting range of this jump,
-            // Move on to the starting range of the next jump.
-            if (i == curEnd) {
-                answer++;
-                curEnd = curFar;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            // Initialize all the index position with some max value to denote it is unreachable
+            dp[i] = Integer.MAX_VALUE;
+        }
+        
+        // Make the first position value as 0 as it is the default place where we start
+        dp[0] = 0;
+		// Main condition check goes here.
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j <= i + nums[i] && j< nums.length; j++) {
+                dp[j] = Math.min(dp[j], dp[i] + 1);
             }
         }
-
-        return answer;
+        return dp[nums.length - 1];
     }
 }
