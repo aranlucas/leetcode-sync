f1b62cde6fb3d6cbf607c62726516a26a7746ffa
Lucas <aranlucas@gmail.com>
2023-04-28 01:22:49 +0000

    Sync LeetCode submission - Number of Islands (java)

--- problems/number_of_islands/solution.java
+++ problems/number_of_islands/solution.java
@@ -1,33 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-
-        int islands = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    islands++;
-                    dfs(i, j, grid, visited);
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    count++;
                 }
             }
         }
-
-        return islands;
+        return count;
+        
     }
 
-    private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
-        if (i < 0
-                || j < 0
-                || i >= grid.length
-                || j >= grid[0].length
-                || visited[i][j]
-                || grid[i][j] == '0') {
+    public void dfs(int x, int y, char[][] grid) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] == '0') {
             return;
         }
-        visited[i][j] = true;
-        dfs(i + 1, j, grid, visited);
-        dfs(i, j + 1, grid, visited);
-        dfs(i - 1, j, grid, visited);
-        dfs(i, j - 1, grid, visited);
+        grid[x][y] = '0';
+
+        dfs(x + 1, y, grid);
+        dfs(x , y + 1, grid);
+        dfs(x - 1, y, grid);
+        dfs(x, y - 1, grid);
     }
 }
