492359f35dc6be95bc2f8903416b8a2746cb5e1a
Lucas <aranlucas@gmail.com>
2023-04-28 23:21:58 +0000

    Sync LeetCode submission - Jump Game (java)

--- problems/jump_game/solution.java
+++ problems/jump_game/solution.java
@@ -1,34 +1,19 @@
-enum Index {
-    GOOD,
-    BAD,
-    UNKNOWN
-}
-
 class Solution {
-
-    public boolean canJumpFromPosition(int position, int[] nums, Index[] memo) {
-        if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD ? true : false;
-        }
-
-        int furthestJump = Math.min(position + nums[position], nums.length - 1);
-        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-            if (canJumpFromPosition(nextPosition, nums, memo)) {
-                memo[position] = Index.GOOD;
-                return true;
+    public boolean canJump(int[] nums) {
+        boolean[] dp = new boolean[nums.length];
+        dp[nums.length - 1] = true;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] ==0) {
+                continue;
+            }
+            for (int j = i + 1; j <= nums[i] + i; j++) {
+                if (dp[j]) {
+                    dp[i] = dp[j];
+                    break;
+                }
             }
         }
 
-        memo[position] = Index.BAD;
-        return false;
-    }
-
-    public boolean canJump(int[] nums) {
-        Index[] memo = new Index[nums.length];
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = Index.UNKNOWN;
-        }
-        memo[memo.length - 1] = Index.GOOD;
-        return canJumpFromPosition(0, nums, memo);
+        return dp[0];
     }
 }
