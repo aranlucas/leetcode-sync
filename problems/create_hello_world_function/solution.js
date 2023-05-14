80054c2520d451b64bef48d51c2fc453b4bd6ce1
Lucas <aranlucas@gmail.com>
2023-05-14 18:24:58 +0000

    Sync LeetCode submission - Create Hello World Function (javascript)

--- problems/create_hello_world_function/solution.js
+++ problems/create_hello_world_function/solution.js
@@ -3,7 +3,7 @@
  */
 var createHelloWorld = function() {
     return function(...args) {
-        return "H" + "e" + "l" + "l" + "o" + " " + "W" + "o" + "r" + "l" + "d"
+        return "Hello World"
     }
 };
 
