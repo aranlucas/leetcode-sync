cf1a62d210e375830c2f0208d0baef4b998f44c2
Lucas <aranlucas@gmail.com>
2023-05-16 01:29:24 +0000

    Sync LeetCode submission - Swap Nodes in Pairs (java)

--- problems/swap_nodes_in_pairs/solution.java
+++ problems/swap_nodes_in_pairs/solution.java
@@ -1,23 +1,39 @@
 /**
- * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {}
- * ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
- * this.next = next; } }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
 
-        if (head == null || head.next == null) {
-            return head;
+        ListNode prevNode = dummy;
+        ListNode cur = head;
+
+        while (cur != null && cur.next != null) {
+            // Nodes to be swapped
+            ListNode node1 = cur;
+            ListNode node2 = cur.next;
+
+            // Swap values
+            prevNode.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = node1;
+            // jump to next
+            cur = node1.next; // ...[...]-->[node2]-->[node1/prev]-->[next]...
         }
 
-        ListNode first = head;
-        ListNode second = head.next;
-
-        ListNode swapped = swapPairs(second.next);
-
-        first.next = swapped;
-        second.next = first;
-
-        return second;
+        // Return new head
+        return dummy.next;
     }
 }
