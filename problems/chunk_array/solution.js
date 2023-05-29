fd82a2317094ce82fe2322b9ac3ddd44f48203ec
Lucas <aranlucas@gmail.com>
2023-05-29 23:39:39 +0000

    Sync LeetCode submission - Chunk Array (javascript)

---
+++ problems/chunk_array/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    const chunked = [];
+
+    let index = 0;
+
+    while (index < arr.length) {
+        chunked.push(arr.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+};
