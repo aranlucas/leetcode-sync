f5426b7e5d090e38d3d602f2e22c7a7fa9880cdf
Lucas <aranlucas@gmail.com>
2023-06-07 21:59:23 +0000

    Sync LeetCode submission - Toss Strange Coins (java)

---
+++ problems/toss_strange_coins/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double probabilityOfHeads(double[] prob, int target) {
+        int n = prob.length;
+        double[][] memo = new double[n][target + 1];
+
+        for (double[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return findProbability(0, n, memo, prob, target);
+    }
+
+    public double findProbability(int index, int n, double[][] memo, double[] prob, int target) {
+        if (target < 0) {
+            return 0;
+        }
+
+        if (index == n) {
+            return target == 0 ? 1 : 0;
+        }
+
+        if (memo[index][target] != -1) {
+            return memo[index][target];
+        }
+
+        double prob1 = findProbability(index + 1, n, memo, prob, target -1) * prob[index];
+        double prob2 = findProbability(index + 1, n, memo, prob, target) * (1 - prob[index]);
+        return memo[index][target] = prob1 + prob2;
+    }
+}
