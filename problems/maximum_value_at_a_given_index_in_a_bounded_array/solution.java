ef1aeda61fc25394cec3f31e5161b681b9254302
Lucas <aranlucas@gmail.com>
2023-06-10 03:22:50 +0000

    Sync LeetCode submission - Maximum Value at a Given Index in a Bounded Array (java)

---
+++ problems/maximum_value_at_a_given_index_in_a_bounded_array/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxValue(int n, int index, int maxSum) {
+        maxSum = maxSum - n;
+
+        int left = 0;
+        int right = maxSum;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (test(n, index, mid) <= maxSum) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+    
+    private long test(int n, int index, int a) {
+        int b = Math.max(a - index, 0);
+        long res = (long)(a + b) * (a - b + 1) / 2;
+        b = Math.max(a - ((n - 1) - index), 0);
+        res += (long)(a + b) * (a - b + 1) / 2;
+        return res - a;
+    }
+}
