8eb906bc826a5d1cfa9e9b778cd489a31f6f8b90
Lucas <aranlucas@gmail.com>
2023-06-13 22:59:27 +0000

    Sync LeetCode submission - 3Sum (java)

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
 
+        Set<List<Integer>> intermediate= new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            twoSum(nums, i, set);
-        }
+            int lo = i + 1;
+            int hi = nums.length - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
 
-        List<List<Integer>> result = new ArrayList<>(set);
-
-        return result;
-    }
-
-    public void twoSum(int[] nums, int i, Set<List<Integer>> set) {
-        int lo = i + 1, hi = nums.length - 1;
-        while (lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-            if (sum < 0) {
-                lo++;
-            } else if (sum > 0) {
-                hi--;
-            } else if (sum == 0) {
-                set.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                lo++;
-                hi--;
+                if (sum == 0) {
+                    List<Integer> r = new ArrayList<>();
+                    r.add(nums[i]);
+                    r.add(nums[lo++]);
+                    r.add(nums[hi--]);
+                    intermediate.add(r);
+                } else if (sum < 0) {
+                    lo++;
+                } else {
+                    hi--;
+                }
             }
         }
+
+        return new ArrayList<>(intermediate);
     }
 }
