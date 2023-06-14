477b4b7586f89dac2ffd29e01d454c209bd1db41
Lucas <aranlucas@gmail.com>
2023-06-14 22:09:40 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- problems/valid_parentheses/solution.java
+++ problems/valid_parentheses/solution.java
@@ -1,23 +1,25 @@
 class Solution {
-    Map<Character, Character> p =
-            Map.of(
-                    '}', '{',
-                    ']', '[',
-                    ')', '(');
+
+    Map<Character, Character> matching = Map.of(
+        '}', '{',
+        ')', '(',
+        ']', '['
+    );
 
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
 
         for (char c : s.toCharArray()) {
-            if (p.containsKey(c)) {
-                if (stack.isEmpty() || stack.pop() != p.get(c)) {
+            if (!stack.isEmpty() && matching.containsKey(c)) {
+                char match = stack.pop();
+
+                if (match != matching.get(c)) {
                     return false;
                 }
             } else {
                 stack.push(c);
             }
         }
-
         return stack.isEmpty();
     }
 }
