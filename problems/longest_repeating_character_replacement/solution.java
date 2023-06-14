9b368af108709d3dfa1db9b653c14edf15034f89
Lucas <aranlucas@gmail.com>
2023-06-14 22:05:48 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (java)

--- problems/longest_repeating_character_replacement/solution.java
+++ problems/longest_repeating_character_replacement/solution.java
@@ -2,23 +2,24 @@ class Solution {
     public int characterReplacement(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int answer = 0;
         int maxFreq = 0;
+        int left = 0;
+        int longestSubstringLength = 0;
         for (int right = 0; right < s.length(); right++) {
             char r = s.charAt(right);
+
             map.put(r, map.getOrDefault(r, 0) + 1);
-            maxFreq = Math.max(map.get(r), maxFreq);
+            maxFreq = Math.max(maxFreq, map.get(r));
 
             while (right - left + 1 - maxFreq > k) {
                 char l = s.charAt(left);
-                map.put(l, map.getOrDefault(l, 0) - 1);
+                map.put(l, map.get(l) - 1);
                 left++;
             }
 
-            answer = Math.max(answer, right - left + 1);
+            longestSubstringLength = Math.max(right - left + 1, longestSubstringLength);
         }
 
-        return answer;
+        return longestSubstringLength;
     }
 }
