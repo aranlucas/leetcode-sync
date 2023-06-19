e755ed111d8f3528bd32955d9a161ea1d00e19b2
Lucas <aranlucas@gmail.com>
2023-06-19 22:24:45 +0000

    Sync LeetCode submission - Find the Highest Altitude (java)

---
+++ problems/find_the_highest_altitude/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        
+        int currentAltitude = 0;
+        int maxAltitude = currentAltitude;
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+
+        return maxAltitude;
+    }
+}
