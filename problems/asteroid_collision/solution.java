13e1fb906c2632721d17f15c95cd5d491a1e573b
Lucas <aranlucas@gmail.com>
2023-06-27 05:22:06 +0000

    Sync LeetCode submission - Asteroid Collision (java)

---
+++ problems/asteroid_collision/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int asteroid: asteroids) {
+            if (asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                // 4 scenarios: 1.+ + 2.- - 3.+ - 4.- +
+                // Collision only at + -
+
+                // Scenario + - one is destroyed
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroid) ) {                  
+                    // destroy the previous positive one(s) 
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                } else if (Math.abs(stack.peek()) == Math.abs(asteroid)) {
+                    stack.pop();
+                }
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+
+        int i = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            answer[i] = stack.pop();
+            i--;
+        }
+        return answer;
+    }
+}
