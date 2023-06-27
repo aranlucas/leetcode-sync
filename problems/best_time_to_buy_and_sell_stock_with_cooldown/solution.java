680e223fbcde622ede8c83cb6a3579d119485397
Lucas <aranlucas@gmail.com>
2023-06-27 20:07:24 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (java)

--- problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.java
+++ problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.java
@@ -1,21 +1,35 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] dp = new int[prices.length + 2];
+      Integer[][][] memo = new Integer[prices.length][2][2];
+      return solve(0, 0, 0, prices, memo);
+    }
 
-        for (int i = prices.length - 1; i >= 0; i--) {
-            int C1 = 0;
-            // Case 1). buy and sell the stock
-            for (int sell = i + 1; sell < prices.length; sell++) {
-                int profit = (prices[sell] - prices[i]) + dp[sell + 2];
-                C1 = Math.max(profit, C1);
-            }
 
-            // Case 2). do no transaction with the stock p[i]
-            int C2 = dp[i + 1];
+    public int solve(int index, int cooldown, int holding, int[] prices, Integer[][][] memo) {
+      if (index == prices.length) {
+        return 0;
+      }
 
-            // wrap up the two cases
-            dp[i] = Math.max(C1, C2);
+      if (memo[index][cooldown][holding] != null) {
+        return memo[index][cooldown][holding];
+      }
+
+      int doNothing = solve(index + 1, 0, holding, prices, memo);
+
+
+      // holding 0 => means you need to buy stock
+      if (holding == 0) {
+        // Check cooldown
+        if (cooldown == 0) {
+          int buyStock = -prices[index] + solve(index + 1, 0, 1, prices, memo);
+
+          return memo[index][cooldown][holding] = Math.max(buyStock, doNothing);
         }
-        return dp[0];
+
+        return doNothing;
+      } else {
+        int sellStock = prices[index] + solve(index + 1, 1, 0, prices, memo);
+        return memo[index][cooldown][holding] = Math.max(doNothing,  sellStock);
+      }
     }
 }
