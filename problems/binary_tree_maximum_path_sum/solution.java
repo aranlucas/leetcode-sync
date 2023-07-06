7407c92037edba07caec033ac02399384f9b322d
Lucas <aranlucas@gmail.com>
2023-07-06 04:30:00 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (java)

--- problems/binary_tree_maximum_path_sum/solution.java
+++ problems/binary_tree_maximum_path_sum/solution.java
@@ -1,30 +1,49 @@
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
- * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
- * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
 class Solution {
-
-    int maxSum = Integer.MIN_VALUE;
-
-    public int helper(TreeNode node) {
-        if (node == null) {
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
             return 0;
         }
+        int result = Integer.MIN_VALUE;
 
-        int leftGain = Math.max(helper(node.left), 0);
-        int rightGain = Math.max(helper(node.right), 0);
+        Deque<TreeNode> postorder = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
 
-        int newSum = node.val + leftGain + rightGain;
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            postorder.push(curr);
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+        }
 
-        maxSum = Math.max(maxSum, newSum);
+        Map<TreeNode, Integer> maxRootPath = new HashMap<>();
+        
+        for (TreeNode node : postorder) {
+            int left = Math.max(maxRootPath.getOrDefault(node.left, 0), 0);
+            int right = Math.max(maxRootPath.getOrDefault(node.right, 0), 0);
+            maxRootPath.put(node, Math.max(left, right) + node.val);
+            result = Math.max(left + right + node.val, result);
+        }
 
-        return node.val + Math.max(leftGain, rightGain);
-    }
-
-    public int maxPathSum(TreeNode root) {
-        helper(root);
-
-        return maxSum;
+        return result;
     }
 }
