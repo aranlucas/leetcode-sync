046843dc306b3a44251a52beace0ea415142c225
Lucas <aranlucas@gmail.com>
2023-07-06 04:37:30 +0000

    Sync LeetCode submission - Design File System (java)

---
+++ problems/design_file_system/solution.java
@@ -0,0 +1,32 @@
+class FileSystem {
+    Map<String, Integer> map;
+
+    public FileSystem() {
+        map = new HashMap<>();
+        map.put("", -1);
+    }
+    
+    public boolean createPath(String path, int value) {
+        if (map.containsKey(path)) {
+            return false;
+        }
+        String parentPath = path.substring(0, path.lastIndexOf("/"));
+        if (!map.containsKey(parentPath)) {
+            return false;
+        }
+
+        map.put(path, value);
+        return true;
+    }
+    
+    public int get(String path) {
+        return map.getOrDefault(path, -1);
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
