369f7aeb4ea4ec0555cb6209cefe445a9f90910a
Lucas <aranlucas@gmail.com>
2023-07-06 05:13:07 +0000

    Sync LeetCode submission - Buddy Strings (java)

--- problems/buddy_strings/solution.java
+++ problems/buddy_strings/solution.java
@@ -1,29 +1,45 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
-        if (s.length() != goal.length()) return false;
-        if (s.equals(goal)) {
-            int[] count = new int[26];
-            for (int i = 0; i < s.length(); ++i)
-                count[s.charAt(i) - 'a']++;
-
-            for (int c: count)
-                if (c > 1) return true;
+        if (s.length() != goal.length()) {
             return false;
-        } else {
-            int first = -1, second = -1;
+        }
+        if (s.equals(goal)) {
+            // If we have 2 same characters in string 's',
+            // we can swap them and still the strings will remain equal.
+            int[] count = new int[26];
             for (int i = 0; i < s.length(); ++i) {
-                if (s.charAt(i) != goal.charAt(i)) {
-                    if (first == -1)
-                        first = i;
-                    else if (second == -1)
-                        second = i;
-                    else
-                        return false;
+                count[s.charAt(i) - 'a']++;
+                if (count[s.charAt(i) - 'a'] == 2) {
+                    return true;
                 }
             }
 
-            return (second != -1 && s.charAt(first) == goal.charAt(second) &&
-                    s.charAt(second) == goal.charAt(first));
+            return false;
+        } 
+        int first = -1;
+        int second = -1;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                if (first == -1) {
+                    first = i;
+                } else if (second == -1) {
+                    second = i;
+                } else {
+                    // We have at least 3 indices with different characters,
+                    // thus, we can never make the strings equal with only one swap.
+                    return false;
+                }
+            }
         }
+
+        if (second == -1) {
+            // We can't swap if the character at only one index is different.
+            return false;
+        }
+
+        // All characters of both strings are the same except two indices.
+        return s.charAt(first) == goal.charAt(second) &&
+                s.charAt(second) == goal.charAt(first);
     }
 }
