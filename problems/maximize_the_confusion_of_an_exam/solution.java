9a0ca0ac91468632f2e0fd637a663e2d8612b4b8
Lucas <aranlucas@gmail.com>
2023-07-07 04:44:37 +0000

    Sync LeetCode submission - Maximize the Confusion of an Exam (java)

---
+++ problems/maximize_the_confusion_of_an_exam/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+
+
+        Map<Character, Integer> count = new HashMap<>();
+        int maxFreq = 0;
+        int left = 0;
+
+        int consecutive = 0;
+        for (int right = 0; right < answerKey.length(); right++) {
+            char r = answerKey.charAt(right);
+
+            count.put(r, count.getOrDefault(r, 0) + 1);
+
+            maxFreq = Math.max(count.get(r), maxFreq);
+
+            while (right - left + 1 - maxFreq > k) {
+                char l = answerKey.charAt(left);
+                count.put(l, count.get(l) - 1);
+                left++;
+            }
+            consecutive = Math.max(consecutive, right - left + 1);
+        }
+
+        return consecutive;
+    }
+}
