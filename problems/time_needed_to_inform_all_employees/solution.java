f8dd19764ef523fa5cd2c4abdae95ddfd0306b1a
Lucas <aranlucas@gmail.com>
2023-07-09 20:59:40 +0000

    Sync LeetCode submission - Time Needed to Inform All Employees (java)

--- problems/time_needed_to_inform_all_employees/solution.java
+++ problems/time_needed_to_inform_all_employees/solution.java
@@ -1,23 +1,32 @@
 class Solution {
+    record Pair(int id, int time) {}
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
         for (int i = 0; i < n; i++) {
-            var map = adj.computeIfAbsent(manager[i], k -> new ArrayList<>());
-            if (manager[i] != -1) {
-                map.add(i);
+            int curManager = manager[i];
+            if (curManager >= 0) {
+                graph.computeIfAbsent(curManager, k -> new ArrayList<>());
+                graph.get(curManager).add(i);
             }
         }
+        
+        Deque<Pair> employee = new ArrayDeque<>();
+        
+        employee.addLast(new Pair(headID, 0));
+        
+        int minutes = 0;
+        while (!employee.isEmpty()) {
+            Pair curr = employee.removeFirst();
+            minutes = Math.max(minutes, curr.time);
 
-        return dfs(adj, informTime, headID);
-    }
-
-    int dfs(Map<Integer, List<Integer>> adj, int[] informTime, int curr) {
-        int maxTime = 0;
-
-        for (int nei : adj.getOrDefault(curr, new ArrayList<>())) {
-            maxTime = Math.max(dfs(adj, informTime, nei), maxTime);
+            for (int sub: graph.getOrDefault(curr.id, new ArrayList<>())) {
+                employee.addLast(new Pair(sub, curr.time + informTime[curr.id]));
+            }
         }
-
-        return maxTime + informTime[curr];
+                                 
+        return minutes;
     }
 }
