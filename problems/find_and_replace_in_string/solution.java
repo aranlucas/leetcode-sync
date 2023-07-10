400247d151670603377c13c5cb03ed66243a0c84
Lucas <aranlucas@gmail.com>
2023-07-10 03:41:30 +0000

    Sync LeetCode submission - Find And Replace in String (java)

---
+++ problems/find_and_replace_in_string/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        
+        Map<Integer, Integer> table = new HashMap<>();
+        
+        for (int i = 0; i < indices.length; i++) {
+            if (s.startsWith(sources[i], indices[i])) {
+                table.put(indices[i], i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < s.length();) {
+           if (table.containsKey(i)) {
+                // if a replacement was recorded before
+                sb.append(targets[table.get(i)]);
+                i += sources[table.get(i)].length();
+            } else {
+                // if no replacement happened at this index
+                sb.append(s.charAt(i));
+                i++;
+           }
+        }
+        
+        return sb.toString();
+    }
+}
