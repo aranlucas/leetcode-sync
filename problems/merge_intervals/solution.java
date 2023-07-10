3f24501d4f1f48e645f5e61974613174079b7549
Lucas <aranlucas@gmail.com>
2023-07-10 03:57:06 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,35 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        List<int[]> res = new ArrayList();
-
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        res.add(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            int[] prevInterval = res.get(res.size() - 1);
-            if (isOverlap(intervals[i], prevInterval)) {
-                // merge interval.
-                // Pop latest rest, insert new interval
+        Deque<int[]> merged = new ArrayDeque<>();
+        merged.push(intervals[0]);
+        for (int i = 1; i < intervals.length; i++){
+            int[] interval = intervals[i];
+            int[] last = merged.getLast();
 
-                int[] newInterval = mergeInterval(intervals[i], prevInterval);
-                res.remove(res.size() - 1);
-                res.add(newInterval);
+            // interval end time less than new interval start - no overlap
+            if (last[1] < interval[0]) {
+                merged.addLast(interval);
             } else {
-                res.add(intervals[i]);
+                // update end time to new end time
+                merged.getLast()[1] = Math.max(last[1], interval[1]);
             }
+
         }
 
-        return res.toArray(new int[res.size()][]);
-    }
-
-    public boolean isOverlap(int[] a, int[] b) {
-        return a[0] <= b[1] && b[0] <= a[1];
-    }
-
-    public int[] mergeInterval(int[] a, int[] b) {
-        int start = Math.min(a[0], b[0]);
-        int end = Math.max(a[1], b[1]);
-        return new int[] {start, end};
+        return merged.toArray(new int[merged.size()][]);
     }
 }
