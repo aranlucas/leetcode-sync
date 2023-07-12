4003bbf2db44ab1aab1bc70df87a07b3e31500c6
Lucas <aranlucas@gmail.com>
2023-07-12 23:55:22 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- problems/valid_sudoku/solution.java
+++ problems/valid_sudoku/solution.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> seen = new HashSet<>();
 
-
-
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
                 char curr = board[r][c];
