39ff644cba1e857d6751ad9bd77ec5c41c311fee
Lucas <aranlucas@gmail.com>
2023-07-12 23:55:08 +0000

    Sync LeetCode submission - Plus One (java)

--- problems/plus_one/solution.java
+++ problems/plus_one/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {          
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] == 9) {
                 digits[i] = 0;
             } else {
@@ -8,9 +9,9 @@ class Solution {
                 return digits;
             }
         }
-
         digits = new int[digits.length + 1];
         digits[0] = 1;
+        
         return digits;
     }
 }
