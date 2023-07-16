16de93fab22a8100657bf414ebbc278ce5dc5c48
Lucas <aranlucas@gmail.com>
2023-07-16 21:27:12 +0000

    Sync LeetCode submission - Largest Color Value in a Directed Graph (java)

--- problems/largest_color_value_in_a_directed_graph/solution.java
+++ problems/largest_color_value_in_a_directed_graph/solution.java
@@ -1,57 +1,47 @@
 class Solution {
-    public int largestPathValue(String colors, int[][] edges) {
+  public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
-
         Map<Integer, List<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[n];
 
         for (int[] edge : edges) {
-            adj.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            int from = edge[0];
+            int to = edge[1];
+            adj.computeIfAbsent(from, k -> new ArrayList<Integer>()).add(to);
+            indegree[to]++;
         }
 
         int[][] count = new int[n][26];
-        boolean[] visit = new boolean[n];
-        boolean[] inStack = new boolean[n];
-        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
 
+        // Push all the nodes with indegree zero in the queue.
         for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, dfs(i, colors, adj, count, visit, inStack));
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
         }
 
-        return answer == Integer.MAX_VALUE ? -1 : answer;
-    }
+        int answer = 1;
+        int nodesSeen = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            answer = Math.max(answer, ++count[node][colors.charAt(node) - 'a']);
+            nodesSeen++;
 
-    private int dfs(
-            int node,
-            String colors,
-            Map<Integer, List<Integer>> adj,
-            int[][] count,
-            boolean[] visit,
-            boolean[] inStack) {
-        if (inStack[node]) {
-            return Integer.MAX_VALUE;
-        }
-
-        if (visit[node]) {
-            return count[node][colors.charAt(node) - 'a'];
-        }
-
-        visit[node] = true;
-        inStack[node] = true;
-
-        if (adj.containsKey(node)) {
-            for (int neighbor : adj.get(node)) {
-                if (dfs(neighbor, colors, adj, count, visit, inStack) == Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
-                }
+            for (int neighbor : adj.getOrDefault(node, new ArrayList<>())) {
                 for (int i = 0; i < 26; i++) {
-                    count[node][i] = Math.max(count[node][i], count[neighbor][i]);
+                    // Try to update the frequency of colors for the neighbor to include paths
+                    // that use node->neighbor edge.
+                    count[neighbor][i] = Math.max(count[neighbor][i], count[node][i]);
+                }
+
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.offer(neighbor);
                 }
             }
         }
 
-        count[node][colors.charAt(node) - 'a']++;
-        // Remove the node from the stack.
-        inStack[node] = false;
-        return count[node][colors.charAt(node) - 'a'];
+        return nodesSeen < n ? -1 : answer;
     }
 }
