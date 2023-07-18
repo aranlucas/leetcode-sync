230d70d3cbda139d26b7f57527e58009843cd961
Lucas <aranlucas@gmail.com>
2023-07-18 20:52:41 +0000

    Sync LeetCode submission - All Paths From Source to Target (java)

--- problems/all_paths_from_source_to_target/solution.java
+++ problems/all_paths_from_source_to_target/solution.java
@@ -1,31 +1,25 @@
 class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    public List<List<Integer>> allPathsSourceTarget(int[][] grid) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
 
-        int target = graph.length - 1;
-        List<List<Integer>> result = new ArrayList<>();
-        LinkedList<Integer> path = new LinkedList<>();
-        path.addLast(0);
 
-        backtrack(0, path, graph, target, result);
+        Deque<List<Integer>> q = new ArrayDeque<>();
+        path.add(0);
+        q.add(path);
 
-        return result;
-    }
-
-    public void backtrack(
-            int currNode,
-            LinkedList<Integer> path,
-            int[][] graph,
-            int target,
-            List<List<Integer>> result) {
-        if (currNode == target) {
-            result.add(new ArrayList<Integer>(path));
-            return;
+        while (!q.isEmpty()) {
+            List<Integer> currentPath = q.removeFirst();
+            int cur = currentPath.get(currentPath.size() - 1);
+            if (cur == grid.length - 1) {
+                answer.add(currentPath);
+            }
+            for (int nei: grid[cur]) {
+                List<Integer> tmpPath = new ArrayList<>(currentPath);
+                tmpPath.add(nei);
+                q.add(tmpPath);
+            }
         }
-
-        for (int nextNode : graph[currNode]) {
-            path.add(nextNode);
-            backtrack(nextNode, path, graph, target, result);
-            path.removeLast();
-        }
+        return answer;
     }
 }
