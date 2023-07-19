4cb3d429dd0f90917612d2e9f772b16f2d6f9dd0
Lucas <aranlucas@gmail.com>
2023-07-19 18:08:04 +0000

    Sync LeetCode submission - Min Cost to Connect All Points (java)

--- problems/min_cost_to_connect_all_points/solution.java
+++ problems/min_cost_to_connect_all_points/solution.java
@@ -1,57 +1,59 @@
+record Pair(int distance, int node) {}
+
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-
-        Map<Integer, List<Pair<Integer, Integer>>> graph = new HashMap();
-
+        
+        Map<Integer, List<Pair>> graph = new HashMap<>();
+        
         // create a weighted graph
         for (int i = 0; i < points.length; i++) {
-            graph.put(i, new ArrayList<Pair<Integer, Integer>>());
+            graph.put(i, new ArrayList<Pair>());
         }
-
-        for (int i = 0; i < points.length - 1; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                int distance =
-                        calculateManhattan(points[i][0], points[j][0], points[i][1], points[j][1]);
-
+        
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i; j < points.length; j++) {
+                if (i == j ) {
+                    continue;
+                }
+                int distance = calculateManhattan(points[i][0], points[j][0], points[i][1], points[j][1]);
+    
                 graph.get(i).add(new Pair(distance, j));
                 graph.get(j).add(new Pair(distance, i));
             }
         }
-
+        
         // edge weight, the index of next node
-        PriorityQueue<Pair<Integer, Integer>> pq =
-                new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.distance() - b.distance());
 
         int len = points.length;
 
         boolean[] visited = new boolean[len];
         int cost = 0;
         pq.offer(new Pair(0, 0));
-
+        
         while (!pq.isEmpty()) {
-            Pair<Integer, Integer> p = pq.poll();
+            Pair p = pq.poll();
+            
+            Integer currNode = p.node();
 
-            Integer weight = p.getKey();
-            Integer currNode = p.getValue();
-
-            if (visited[currNode]) continue;
+            if (visited[currNode]) {
+                continue;
+            }
 
             visited[currNode] = true;
-            cost += weight;
-
-            for (Pair<Integer, Integer> nextNodePair : graph.get(currNode)) {
-                Integer nextWeight = nextNodePair.getKey();
-                Integer nextNode = nextNodePair.getValue();
-                if (!visited[nextNode]) {
-                    pq.offer(new Pair(nextWeight, nextNode));
+            cost += p.distance();
+            
+            for (Pair nextNodePair: graph.get(currNode)) {
+                if (!visited[nextNodePair.node()]) {
+                    pq.offer(nextNodePair);
                 }
             }
         }
-
+        
         return cost;
     }
-
+    
     private int calculateManhattan(int x1, int x2, int y1, int y2) {
-        return Math.abs(x2 - x1) + Math.abs(y2 - y1);
+        return Math.abs(x2-x1) + Math.abs(y2-y1);
     }
 }
