1c3777e0bbb4634b28809044b54333b1e7589a6f
Lucas <aranlucas@gmail.com>
2023-07-19 02:50:05 +0000

    Sync LeetCode submission - Construct the Lexicographically Largest Valid Sequence (java)

---
+++ problems/construct_the_lexicographically_largest_valid_sequence/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] constructDistancedSequence(int n) {
+        int[] sol = new int[(2 * n) - 1];
+        boolean[] visited = new boolean[n + 1];
+        backtrack(0, sol, visited, n);
+        return sol;
+    }
+
+    private boolean backtrack(int i, int[] sol, boolean[] visited, int n) {
+        if (i == sol.length) {
+            return true;
+        }
+
+        if (sol[i] != 0) {
+            return backtrack(i + 1, sol, visited, n);
+        }
+
+        for(int j = n; j > 0; j--) {
+            if (!visited[j] && (j == 1 || i + j < sol.length && sol[i + j] == 0)) {
+                sol[i] = j;
+                if (j != 1) {
+                    sol[i + j] = j;
+                }
+
+                visited[j] = true;
+
+                if (backtrack(i + 1, sol, visited, n)) {
+                    return true;
+                }
+                
+                sol[i] = 0;
+
+                if (j != 1) {
+                    sol[i + j] = 0;
+                }
+
+                visited[j] = false;
+            }
+        }
+        return false;
+    }
+}
