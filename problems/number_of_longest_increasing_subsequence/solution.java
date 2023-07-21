a6c39b0d4eb0add5ed0f0ec5504da5e9d60552c5
Lucas <aranlucas@gmail.com>
2023-07-21 22:40:16 +0000

    Sync LeetCode submission - Number of Longest Increasing Subsequence (java)

---
+++ problems/number_of_longest_increasing_subsequence/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+
+        //  j  i
+        // [1, 2, 3]
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = 0;
+                    }
+
+                    if (dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+        }
+        
+        int longest = 0;
+        for (int c: dp) {
+            longest = Math.max(longest, c);
+        }
+        
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (dp[i] == longest) {
+                result += count[i];
+            }
+        }   
+
+        return result;
+    }
+}
