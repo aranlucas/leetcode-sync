83217d65ef096d54df9162f5137a7f5cf72cabd8
Lucas <aranlucas@gmail.com>
2023-07-22 00:21:34 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- problems/longest_increasing_subsequence/solution.java
+++ problems/longest_increasing_subsequence/solution.java
@@ -1,28 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        Integer[][] memo = new Integer[nums.length][nums.length];
-        return solve(-1, 0, nums, memo);
-    }
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
 
-    public int solve(int prev, int curr, int[] nums, Integer[][] memo) {
-        if (curr == nums.length) {
-            return 0;
+        //  j  i
+        // [10,9,2,5,3,7,101,18]
+        for (int cur = 1; cur < nums.length; cur++) {
+            for (int prev = 0; prev < cur; prev++) {
+                if (nums[cur] > nums[prev]) {
+                    if (dp[cur] < dp[prev] + 1) {
+                        dp[cur] = dp[prev] + 1;
+                    }
+                }
+            }
         }
-
-        if (memo[prev + 1][curr] != null) {
-            return memo[prev + 1][curr];
+        
+        int longest = 0;
+        for (int c: dp) {
+            longest = Math.max(longest, c);
         }
-
-        int take = 0;
-        if (prev == -1 || nums[prev] < nums[curr]) {
-            // Use curr in sequence
-            take = 1 + solve(curr, curr + 1, nums, memo);
-        }
-
-        // Don't use this number in sequence
-        int noTake = solve(prev, curr + 1, nums, memo);
-
-        int answer = Math.max(take, noTake);
-        return memo[prev + 1][curr] = answer;
+        
+        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.toString(count));
+        return longest;
     }
 }
