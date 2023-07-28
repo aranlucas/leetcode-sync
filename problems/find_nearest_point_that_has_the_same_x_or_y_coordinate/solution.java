a981156feea7bfdd9ea16731bb697559dc2e7b9a
Lucas <aranlucas@gmail.com>
2023-07-28 20:57:03 +0000

    Sync LeetCode submission - Find Nearest Point That Has the Same X or Y Coordinate (java)

--- problems/find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.java
+++ problems/find_nearest_point_that_has_the_same_x_or_y_coordinate/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int nearestValidPoint(int x, int y, int[][] points) {
-        int min = Integer.MAX_VALUE;
-        int answer = -1;
 
+
+        int distance = Integer.MAX_VALUE;
+        int answer = -1;
         for (int i = 0; i < points.length; i++) {
             int[] point = points[i];
             if (x == point[0] || y == point[1]) {
-                int distance = calculateDistance(x, y, point[0], point[1]);
-
-                if (distance < min) {
-                    min = distance;
+                int curDistance = calculateDistance(x, y, point);
+                if (curDistance < distance) {
+                    distance = curDistance;
                     answer = i;
                 }
             }
@@ -18,7 +18,7 @@ class Solution {
         return answer;
     }
 
-    public int calculateDistance(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    private int calculateDistance(int x, int y, int[] point) {
+        return Math.abs(x - point[0]) + Math.abs(y - point[1]);
     }
 }
