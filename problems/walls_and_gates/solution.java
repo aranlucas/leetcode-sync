f34384f276e6375f0852dced7d543e4c2a3f24ee
Lucas <aranlucas@gmail.com>
2023-07-28 21:30:16 +0000

    Sync LeetCode submission - Walls and Gates (java)

--- problems/walls_and_gates/solution.java
+++ problems/walls_and_gates/solution.java
@@ -1,37 +1,52 @@
 class Solution {
-    private static final int EMPTY = Integer.MAX_VALUE;
-    private static final int GATE = 0;
-    private static final int[][] DIRECTIONS = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    int[][] DIRS = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    int GATE = 0;
+    int EMPTY = Integer.MAX_VALUE;
+    int WALL = -1;
 
     public void wallsAndGates(int[][] rooms) {
-        int m = rooms.length;
-        if (m == 0) {
-            return;
-        }
-        int n = rooms[0].length;
+        int n = rooms.length;
+        int m = rooms[0].length;
+
         Deque<int[]> q = new ArrayDeque<>();
 
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (rooms[row][col] == GATE) {
-                    q.add(new int[] {row, col});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rooms[i][j] == GATE) {
+                    // Begin search from all gates
+                    q.add(new int[] { i, j});
                 }
             }
         }
 
+        int level = 0;
         while (!q.isEmpty()) {
-            int[] point = q.poll();
-            int row = point[0];
-            int col = point[1];
-            for (int[] direction : DIRECTIONS) {
-                int r = row + direction[0];
-                int c = col + direction[1];
-                if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
-                    continue;
+            int size = q.size();
+
+            for (int k = 0; k < size; k++) {
+                int[] cur = q.pop();
+
+                for (int[] dir: DIRS) {
+                    int dx = cur[0] + dir[0];
+                    int dy = cur[1] + dir[1];
+
+                    if (0 <= dx && dx < n && 0 <= dy && dy < m) {
+                        // Skip walls and obstacles
+                        if (rooms[dx][dy] == WALL) {
+                            continue;
+                        }
+                        // Skip if visited already
+                        if (rooms[dx][dy] != EMPTY) {
+                            continue;
+                        }
+
+                        rooms[dx][dy] = level + 1;
+                        q.add(new int[] {dx, dy});
+                    }
                 }
-                rooms[r][c] = rooms[row][col] + 1;
-                q.add(new int[] {r, c});
             }
+            level++;
         }
     }
 }
