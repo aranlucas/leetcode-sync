afdf5dd5383f1d5e8fd9e6185d1d3cfb5c111e17
Lucas <aranlucas@gmail.com>
2023-07-28 22:53:50 +0000

    Sync LeetCode submission - Design Tic-Tac-Toe (java)

--- problems/design_tic-tac-toe/solution.java
+++ problems/design_tic-tac-toe/solution.java
@@ -6,14 +6,16 @@ class TicTacToe {
         this.board = new int[n][n];
         this.n = n;
     }
-
+    
     public int move(int row, int col, int player) {
         board[row][col] = player;
 
-        if (checkRow(row, player)
-                || checkColumn(col, player)
-                || (row == col && checkDiagonal(player))
-                || (col == n - row - 1 && checkAntiDiagonal(player))) {
+        if (
+            checkRow(row, player) || 
+            checkColumn(col, player) ||
+            checkDiagonal(player) || 
+            checkAntiDiagonal(player)       
+        ) {
             return player;
         }
 
@@ -28,7 +30,7 @@ class TicTacToe {
         }
         return true;
     }
-
+    
     private boolean checkAntiDiagonal(int player) {
         for (int row = 0; row < n; row++) {
             if (board[row][n - row - 1] != player) {
@@ -36,7 +38,7 @@ class TicTacToe {
             }
         }
         return true;
-    }
+    }  
 
     private boolean checkColumn(int col, int player) {
         for (int row = 0; row < n; row++) {
