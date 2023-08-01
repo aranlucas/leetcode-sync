02ed0c283d34b8ac11fb005c7c148d326f0f07c8
Lucas <aranlucas@gmail.com>
2023-08-01 00:52:57 +0000

    Sync LeetCode submission - Combinations (java)

---
+++ problems/combinations/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+
+        backtrack(1, n, k, current, answer);
+
+        return answer;
+    }
+
+
+    private void backtrack(int start, int n, int k, List<Integer> current, List<List<Integer>> answer) {
+        if (current.size() == k) {
+            answer.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+            backtrack(i + 1, n, k, current, answer);
+            current.remove(current.size() - 1);
+        }
+    }
+}
