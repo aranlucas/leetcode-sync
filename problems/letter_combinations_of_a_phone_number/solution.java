e5a225ccb88a247cf20394d4296ac076b34f8d80
Lucas <aranlucas@gmail.com>
2023-08-03 01:51:27 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- problems/letter_combinations_of_a_phone_number/solution.java
+++ problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,39 +1,39 @@
 class Solution {
-    Map<Character, List<Character>> charToLetter = new HashMap<>();
+    String[] KEYBOARD = {
+        "", 
+        "", 
+        "abc", 
+        "def", 
+        "ghi", 
+        "jkl", 
+        "mno", 
+        "pqrs", 
+        "tuv", 
+        "wxyz"
+    };
 
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
             return new ArrayList<>();
         }
-        charToLetter.put('1', new ArrayList<>());
-        charToLetter.put('2', List.of('a', 'b', 'c'));
-        charToLetter.put('3', List.of('d', 'e', 'f'));
-        charToLetter.put('4', List.of('g', 'h', 'i'));
-        charToLetter.put('5', List.of('j', 'k', 'l'));
-        charToLetter.put('6', List.of('m', 'n', 'o'));
-        charToLetter.put('7', List.of('p', 'q', 'r', 's'));
-        charToLetter.put('8', List.of('t', 'u', 'v'));
-        charToLetter.put('9', List.of('w', 'x', 'y', 'z'));
-        charToLetter.put('0', new ArrayList<>());
+        List<String> result = new ArrayList<>();
+        backtrack(0, digits, new StringBuilder(), result);
 
-        List<String> answer = new ArrayList<>();
-        StringBuilder curr = new StringBuilder();
-        backtrack(0, answer, digits, curr);
-        return answer;
+        return result;
     }
 
-    private void backtrack(int index, List<String> answer, String digits, StringBuilder curr) {
-        // Are we done?
-        if (index >= digits.length()) {
-            answer.add(curr.toString());
+    private void backtrack(int index, String digits, StringBuilder current, List<String> result) {
+        if (current.length() == digits.length()) {
+            result.add(current.toString());
             return;
         }
 
-        // What are the candidates?
-        for (char candidate : charToLetter.get(digits.charAt(index))) {
-            curr.append(candidate);
-            backtrack(index + 1, answer, digits, curr);
-            curr.deleteCharAt(curr.length() - 1);
+        int c = digits.charAt(index) - '0';
+
+        for (char candidate: KEYBOARD[c].toCharArray()) {
+            current.append(candidate);
+            backtrack(index + 1, digits, current, result);
+            current.deleteCharAt(current.length() - 1);
         }
     }
 }
